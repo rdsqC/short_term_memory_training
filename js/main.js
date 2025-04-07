364473dff6c0b4ee2ff2620ad045215a04256b1c
rdsqC <pdiamondq49@gmail.com>
2025-04-07 17:03:18 +0900

    完成

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let url = new URL(window.location.href);
+let length = url.searchParams.get("length").toString() || 500;
+
+function createRandString(length, chars) {
+  console.log(chars);
+  let result = "";
+  let randIndex;
+  for (let i = 0; i < length; ++i) {
+    randIndex = Math.floor(Math.random() * chars.length);
+    result += chars[randIndex];
+  }
+  return result;
+}
+
+
+const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const text = createRandString(length, chars);
+document.querySelector(".main_text").textContent = text;
