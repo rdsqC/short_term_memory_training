9ae87f64540830c68e4c94086355423ccf96b05e
rdsqC <pdiamondq49@gmail.com>
2025-04-07 17:15:26 +0900

    パラメータがない時に機能しゅないバグを修正

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let url = new URL(window.location.href);
-let length = url.searchParams.get("length").toString() || 500;
+let length_params = url.searchParams.get("length");
+let length = length_params? length_params.toString() : 500;
 
 function createRandString(length, chars) {
   console.log(chars);
